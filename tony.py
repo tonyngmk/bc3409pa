2f1c0df2ddd49025f6355ec7a11ba6c1171b0dbf
Tony Ng <TONY0006@e.ntu.edu.sg>
2020-10-29 13:35:20 +0800

    Repush as due to shap.getjs() error

--- tony.py
+++ tony.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 # from sklearn.ensemble import GradientBoostingRegressor
 # import xgboost
 import shap
